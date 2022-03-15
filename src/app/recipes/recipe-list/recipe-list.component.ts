7dc4f4e62f605062c55680c483f1a57f7a356f16
lorj <juanjose.lorenzosaez@elca.ch>
2022-03-15 09:40:24 +0100

    Initial Commit with all the changes done to the project

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('Tika Masala', 'Grilled lemongrass pork chops with nuoc cham and shredded carrots', 
-    'https://picturetherecipe.com/wp-content/uploads/2020/07/Butter-Chicken-PictureTheRecipe.jpg'),
+    new Recipe(
+      'Tika Masala',
+      'Grilled lemongrass pork chops with nuoc cham and shredded carrots.',
+      'https://picturetherecipe.com/wp-content/uploads/2020/07/Butter-Chicken-PictureTheRecipe.jpg'
+    ),
+
+    new Recipe(
+      'Chicken with Almond',
+      'Chicken with some Almond and you can add more ingredients if you want.',
+      'https://imag.bonviveur.com/presentacion-del-pollo-con-almendras-estilo-chino.webp'
+    ),
   ];
-  
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
-
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -8,10 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
   
-
+  selectedRecipe: Recipe;
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
