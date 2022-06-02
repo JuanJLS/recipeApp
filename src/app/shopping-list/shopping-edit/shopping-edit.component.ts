a5ceb70fd905f14bfeaabc7e2bbe00d647725ba1
lorj <juanjose.lorenzosaez@elca.ch>
2022-06-02 15:58:09 +0200

    Added Set Interval example

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { interval } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -12,6 +13,8 @@ export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput', {static:false}) nameInputRef: ElementRef;
   @ViewChild('amountInput', {static:false}) amountInputRef: ElementRef;
 
+  showAlert: boolean = false;
+
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {}
@@ -20,6 +23,16 @@ export class ShoppingEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
+    this.showAlert = true;
+    this.showAlertInterval();
     this.slService.addIngredient(newIngredient);
+  }
+
+  showAlertInterval() {
+   setInterval(() => {
+      this.showAlert = false;
+   }, 2500)
+
+
   }
 }
