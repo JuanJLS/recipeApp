516d65d8aeca37fa93b75c9ca5277937a7919d3c
lorj <juanjose.lorenzosaez@elca.ch>
2022-06-13 09:34:01 +0200

    Added Routing to the Recipe Project

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,12 +5,5 @@ import { Component, EventEmitter, Output } from "@angular/core";
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-    collapsed: boolean = true;
-    //to pass this event to the parent component(app.component)
-    @Output() featureSelected = new EventEmitter<string>();
-
-    onSelect(feature: string) {
-        this.featureSelected.emit(feature);
-    }
 
 }
