85c137305feb554ba97a082a418aabf9cb6d51d6
lorj <juanjose.lorenzosaez@elca.ch>
2022-06-30 08:53:56 +0200

    Added some styles & Changed form to ngForm

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, OnInit} from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -9,9 +9,6 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  
-  @ViewChild('nameInput', {static:false}) nameInputRef: ElementRef;
-  @ViewChild('amountInput', {static:false}) amountInputRef: ElementRef;
 
   showAlert: boolean = false;
 
@@ -19,10 +16,9 @@ export class ShoppingEditComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.showAlert = true;
     this.showAlertInterval();
     this.slService.addIngredient(newIngredient);
